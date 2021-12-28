57870bd9d71141aa004d52c05b5f5de673168609
tinopai <eltinopa@icloud.com>
2021-12-28 20:16:15 -0300

    feat: 📷 Hide image to prevent overflow

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ window.open = function() {
 `
 
 let script = document.createElement('img');
-script.setAttribute('src','https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png');
+script.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png');
+script.setAttribute('style', 'display:none'); // No need to show it, preventing websites from showing unnecessary overflows
 script.setAttribute('onload', `console.log('Loaded Intercept');${inject}`);
 document.getElementsByTagName('html')[0].appendChild(script);
